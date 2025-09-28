38c89affe7542a873981572abc6adc776f965d5b
olejorgensen <olejorgensen@me.com>
2025-09-28 12:03:01 +0200

    chores

--- Midi/Messages/MessageListExtensions.cs
+++ Midi/Messages/MessageListExtensions.cs
@@ -31,25 +31,4 @@ namespace fractions
             return noteOnMessages;
         }
     }
-
-    public static class NoteMessageListExtensions
-    {
-        public static void SetOctaveAbove<T>(this List<T> messages)
-            where T : NoteMessage
-        {
-            messages?.ForEach(message =>
-            {
-                message?.SetOctaveAbove();
-            });
-        }
-
-        public static void SetOctaveBelow<T>(this List<T> messages)
-            where T : NoteMessage
-        {
-            messages?.ForEach(message =>
-            {
-                message?.SetOctaveBelow();
-            });
-        }
-    }
 }
---
+++ Midi/Messages/NoteMessageListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace fractions
+{
+    public static class NoteMessageListExtensions
+    {
+        public static void SetOctaveAbove<T>(this List<T> messages)
+            where T : NoteMessage
+        {
+            messages?.ForEach(message =>
+            {
+                message?.SetOctaveAbove();
+            });
+        }
+
+        public static void SetOctaveBelow<T>(this List<T> messages)
+            where T : NoteMessage
+        {
+            messages?.ForEach(message =>
+            {
+                message?.SetOctaveBelow();
+            });
+        }
+    }
+}
