15253bfa7ca09f2520a01e4cef755beab8b2ff67
olejorgensen <olejorgensen@me.com>
2025-09-28 10:38:46 +0200

    meh

--- Midi/Extensions/RandomExtensions.cs
+++ Midi/Extensions/RandomExtensions.cs
@@ -4,12 +4,6 @@ namespace fractions
 {
     public static class RandomExtensions
     {
-        /// <summary>
-        /// Returns whether the collection has any members.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <returns>whether the collection has any members</returns>
         public static float NextVelocity(this Random random, float min = 0f, float max = 127f)
         {
             return (float)(((max - min) * random.NextDouble()) + min);
